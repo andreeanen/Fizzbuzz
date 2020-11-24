0eee79da2548640cce850e58f7d2f2f799e8fd1a
andreeanen <andreeanen@yahoo.com>
2020-11-24 12:55:39 +0100

    Remove comments

--- FizzbuzzTests/ProgramTest.cs
+++ FizzbuzzTests/ProgramTest.cs
@@ -106,28 +106,23 @@ namespace FizzbuzzTests
         [TestMethod]
         public void ValidateIntegerInputTest()
         {
-            //Arrange
             var program = new Program();
-            //Act
+
             var expected = program.ValidateIntegerInput("12");
             int integerNumber = 12;
-            //Assert
+
             Assert.AreEqual(integerNumber, expected);
         }
 
         [TestMethod]
         public void ValidateRangeInputTest()
         {
-            //Arrange
+
             var program = new Program();
-            //Act
+
             var expected = program.ValidateRangeInput(100);
-            //Assert
+
             Assert.IsTrue(expected);
         }
-
-
     }
-
-
 }
